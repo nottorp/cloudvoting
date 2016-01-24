00224f760cb1e8e3490fc2d5c7ae5dfafda5eb43
Torp <torp@torp.ro>
2016-01-24 22:52:56 +0200

    Vote plugin has form.

--- impl/plugins/pluginVote.py
+++ impl/plugins/pluginVote.py
@@ -2,9 +2,27 @@ from CCPlugin import CCPlugin
 
 className = "Vote"
 
+form = """
+<html><body><form method="get">
+Your options are:<br/>
+1 - Cake<br/>
+2 - Pudding<br/>
+3 - Candy<br/>
+4 - Lollipop<br/>
+5 - Ion Iliescu<br/>
+Please enter your order of preference, best first, in the field below, then press submit<br/>
+<input name="hierarchy" type="text" value="5"/><br/>
+<input type="submit"/>
+<input type="hidden" name="postme" value="dummy"/>
+</form></body></html>
+"""
+
 class Vote(CCPlugin):
 	def perform(self):
-		return "WIP"
+		if 'postme' in self._request.args:
+			res = self._request.args['hierarchy']
+			return "Received hierarchy: " + str(res)
+		return form
 
 	def shutdown(self):
 		print "Shutting down Borda counter"
